e8156f302a4e69653f7e98afd2e38cc2844800d7
Alison Felix <felix.alison@outlook.com>
2025-04-22 19:15:21 +0100

    Create create-search.sh

---
+++ Labfiles/02-search-skill/create-search/create-search.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# Set values for your Search service
+url="YOUR_SEARCH_URL"
+admin_key="YOUR_ADMIN_KEY"
+
+echo "-----"
+echo "Creating the data source..."
+curl -X POST "${url}/datasources?api-version=2020-06-30" \
+     -H "Content-Type: application/json" \
+     -H "api-key: ${admin_key}" \
+     -d @data_source.json
+
+echo "-----"
+echo "Creating the skillset..."
+curl -X PUT "${url}/skillsets/margies-custom-skillset?api-version=2020-06-30" \
+     -H "Content-Type: application/json" \
+     -H "api-key: ${admin_key}" \
+     -d @skillset.json
+
+echo "-----"
+echo "Creating the index..."
+curl -X PUT "${url}/indexes/margies-custom-index?api-version=2020-06-30" \
+     -H "Content-Type: application/json" \
+     -H "api-key: ${admin_key}" \
+     -d @index.json
+
+# Wait for 3 seconds
+sleep 3
+
+echo "-----"
+echo "Creating the indexer..."
+curl -X PUT "${url}/indexers/margies-custom-indexer?api-version=2020-06-30" \
+     -H "Content-Type: application/json" \
+     -H "api-key: ${admin_key}" \
+     -d @indexer.json
